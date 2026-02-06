b44e51d10c9b0426b2b3e5e4177a66e556ae9882
Jairo Narvaez <jaironarvaez45@gmail.com>
2026-02-06 12:32:50 -0400

    agregar libreria log

--- app/Jobs/TrabajoAutoEnviarWordPressDominio.php
+++ app/Jobs/TrabajoAutoEnviarWordPressDominio.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\Log;
 class TrabajoAutoEnviarWordPressDominio implements ShouldQueue
 {
         use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
